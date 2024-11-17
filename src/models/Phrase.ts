103dfbfa16e80e7b4ea9fcf2fd197256d0f9ca4b
josue <josuealbelaez@gmail.com>
2024-11-17 14:33:26 -0300

    nuevas categories

--- src/models/Phrase.ts
+++ src/models/Phrase.ts
@@ -22,7 +22,12 @@ const phraseSchema = new mongoose.Schema<IPhrase>({
             'Travel and Tourism', 'Family and Personal Relationships', 'Work and Professions',
             'Education and Learning', 'Food and Restaurants', 'Emergencies and Safety',
             'Entertainment and Leisure', 'Technology and Communication', 'Culture and Society',
-            'Opinions and Debates',
+            'Sports and Outdoor Activities', 'Advanced Idioms and Expressions', 'Opinions and Debates',
+            'Environment and Sustainability', 'Professional Networking and Business Jargon',
+            'Psychology and Emotions', 'Literature and Arts', 'Cultural Traditions and Festivals',
+            'Science and Innovation', 'Politics and Current Events', 'History and Historical Events',
+            'Law and Legal Terminology', 'Advanced Debate and Rhetoric', 'Travel for Study or Work Abroad',
+            'Financial and Investment Terminology', 'Philosophy and Ethics', 'Development and Software Engineering',
             // Portuguese categories
             'Cumprimentos e Apresentações', 'Diretrizes Básicas', 'Procedimentos em Escritórios',
             'Família e Casa', 'Saúde e Bem-Estar', 'Compras e Negócios',
